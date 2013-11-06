cf4f61f64b2e45010474a245c41376fadcdf0e04
Timm <kaspth@gmail.com>
2013-11-06 11:11:12 +0100

    Created a template for pages.

---
+++ template.php
@@ -0,0 +1,19 @@
+<?
+# include helpers
+# setup needed variables for the page
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <? include 'includes/head.php'; ?>
+    <title><!-- insert title --></title>
+  </head>
+  <body>
+    <? include 'includes/header.php'; ?>
+
+    <!-- insert markup -->
+
+    <? include 'includes/scripts.php'; ?>
+    <? include 'includes/footer.php'; ?>
+  </body>
+</html>
