1e04945d2e6538ad25e024fe77a76c4f0e2d7ba9
Timm <kaspth@gmail.com>
2013-11-06 10:03:52 +0100

    Added methods to categories_helper.

--- helpers/categories_helper.php
+++ helpers/categories_helper.php
@@ -6,4 +6,16 @@ function fetch_categories() {
   return read_json('db/categories/categories.json');
 }
 
+function fetch_category_by_id($id) {
+  return fetch_categories()[$id];
+}
+
+function article_for_category($category) {
+  $id = $category['id'];
+  return "<article class=\"category\" id=\"{$id}\">
+    <img src=\"assets/category_images/{$id}.jpg\">
+    <span class=\"category-title\">{$category['title']}</span>
+  </article>";
+}
+
 ?>
