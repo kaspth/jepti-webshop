57caeb302b74215aa5cf7a305ab6c145f4a749ab
Timm <kaspth@gmail.com>
2013-11-06 10:04:54 +0100

    Created ability to view products for a category.

--- helpers/products_helper.php
+++ helpers/products_helper.php
@@ -6,4 +6,8 @@ function fetch_products_for_category_id($id) {
   return read_json("db/products/{$id}/products.json");
 }
 
+function image_tag_for_product($product) {
+  return "<img class=\"product-banner-image\" src=\"{$product['image_url']}\" alt=\"{$product['image_alt']}\" />";
+}
+
 ?>
