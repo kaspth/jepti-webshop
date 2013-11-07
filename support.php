4d3c5eb7d624beaeac650d5b326465a467017dfc
Timm <kaspth@gmail.com>
2013-11-07 13:58:08 +0100

    Forgot to add the php version of the support page.

---
+++ support.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'includes/head.php'; ?>
+    <title>Support</title>
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+
+    <section class="support">
+        <h1 class="title">Support</h1>
+        <p>
+            Vi er altid inden for rækkevidde, hvis du har spørgsmål.<br>
+            Skriv til os på <a href="">mail</a><br>
+            Ring til os på <a href="tel:71990905">71990905</a><br>
+            Hverdage mellem kl. <time>09:00</time>-<time>16:00</time>
+        </p>
+
+        <address>
+            JEPTI ApS<br>
+            Usserød Kongevej 80<br>
+            2979 Hørsholm
+        </address>
+    </section>
+
+    <?php include 'includes/scripts.php'; ?>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
