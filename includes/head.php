d84f107a1ad73658fe4b84542f13e2dfcf7033ed
Yevgeniya <masechka1983@gmail.com>
2013-11-08 21:55:03 +0100

    Sublim asked me to reload head

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width" />
 <link href="assets/style.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Armata' rel='stylesheet' type='text/css'>
