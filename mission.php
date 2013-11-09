b06d1754d88178407981aa795eb67c380289bbd1
Timm <kaspth@gmail.com>
2013-11-09 16:23:30 +0100

    Restructured mission page.

--- mission.php
+++ mission.php
@@ -11,15 +11,26 @@
     <h1 class="title">Jeptis mission</h1>
   </header>
   <section class="mission">
-    	<h3 class="subtitle">Hvorfor bruger Jepti?</h3>
-    	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-    	<a href="about_insurance.html">Læs mere om vores forsikring</a>
-    	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-    	<a href="about_security.html">Læs mere om sikkerhed på Jepti</a>
-    	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-    	<a href="about_contracts.html">Læs mere om Jeptis kontrakt</a>
-    	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-    	<a href="http://www.skat.dk/SKAT.aspx?oId=5811&vId=0&lang=DA">Læs mere om dine skattepligtige forhold på Jepti</a><br>
+    <section class="statement">
+      	<h3 class="subtitle">Hvorfor bruge Jepti?</h3>
+      	<p class="clause">Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
+      	<a class="forward-redirect" href="about/insurance.php">Læs mere om vores forsikring</a>
+    </section>
+
+    <section class="statement">
+      <p class="clause">Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
+      <a class="forward-redirect" href="about/security.php">Læs mere om sikkerhed på Jepti</a>
+    </section>
+
+    	<section class="statement">
+      <p class="clause">Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
+      <a class="forward-redirect" href="about/contracts.php">Læs mere om Jeptis kontrakt</a>
+    </section>
+
+    	<section class="statement">
+      <p class="clause">Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
+      <a class="forward-redirect" href="http://www.skat.dk/SKAT.aspx?oId=5811&vId=0&lang=DA">Læs mere om dine skattepligtige forhold på Jepti</a>
+    </section>
 
     <section class="center-section">
       <a class="button" href="faq.php">FAQ</a>
