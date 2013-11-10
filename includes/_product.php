b732e38fcab4709bee9f96026cf8886b05e0d65b
Timm <kaspth@gmail.com>
2013-11-10 19:31:12 +0100

    Restructured product partial.

--- includes/_product.php
+++ includes/_product.php
@@ -1,11 +1,10 @@
 <a class="product-link" href="product.php?id=<?php echo $product['id']; ?>">
   <article class="product group">
     <?php echo image_tag_for_product($product); ?>
-    <h4><?php echo $product['name']; ?></h4>
 
-    <section class="right">
-      <div><em>By:</em> <?php echo $product['city']; ?></div>
-      <div><em>Pris:</em> <?php echo $product['price_per_day']; ?></div>
-    </section>
+    <h4><?php echo $product['name']; ?></h4>
+    <div class="price"><?php echo $product['price_per_day']; ?></div>
+
+    <div class="location right"><?php echo $product['city']; ?></div>
   </article>
 </a>
