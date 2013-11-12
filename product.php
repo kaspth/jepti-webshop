8ca83d67703e2fefedeeff838a8fad8cac1dc5f0
Timm <kaspth@gmail.com>
2013-11-12 18:59:43 +0100

    Added and styled user links on product pages.

--- product.php
+++ product.php
@@ -1,11 +1,13 @@
 <?php
 include_once 'helpers/products_helper.php';
+include_once 'helpers/users_helper.php';
 
 $product_id = $_GET['id'];
 if (!isset($product_id))
   header('Location: index.php');
 
 $product = fetch_product_by_id($product_id);
+$user = fetch_user_by_id($product['user_id']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,10 +23,15 @@ $product = fetch_product_by_id($product_id);
         <h1 class="title"><?php echo $product['name']; ?></h1>
         <?php echo image_tag_for_product($product); ?>
 
+        <a class="user-link right" href="user.php?id=<?php echo $user['id'] ?>">
+          <h3>Uploadet af <?php echo $user['first_name'] ?></h3>
+          <div><?php echo $user['last_name'] ?></div>
+        </a>
+
         <section class="list-information">
           <a href="http://maps.google.com">Vis på kort</a>
-          <div><em>By:</em> <?php echo $product['city']; ?></div>
-          <div><em>Pris per dag:</em> <?php echo $product['price']; ?></div>
+          <div><?php echo $product['city']; ?></div>
+          <div><?php echo $product['price_per_day']; ?> kr./dag</div>
         </section>
       </header>
 
