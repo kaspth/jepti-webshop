067f79301daa08ca85152b16ae7658dde8f63df3
Yevgeniya <masechka1983@gmail.com>
2013-11-13 10:48:17 +0100

    Made footer.php and styled footer_links display: inline

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,30 @@
 <footer>
-  
+	<div class="footer_links">
+	  Om os
+	  <a href="om_os.php"></a>
+	</div>
+
+	<div class="footer_links">
+	  Presse
+	  <a href="presse.php"></a>
+	</div>
+
+	<div class="footer_links">
+	  Brugervilkår
+	  <a href="brugervilkår.php"></a>
+    </div>
+
+    <div class="footer_links">
+      Cookie og privatlivspolitik
+      <a href="cookie.php"></a>
+    </div>
+
+    <div class="footer_links">
+      &copy Jepti 2012
+    </div>
+
+    <div class="footer_links">
+      Usserød Kongevej 80 2970 Hørsholm Telefon: 71990905
+    </div>
+
 </footer>
