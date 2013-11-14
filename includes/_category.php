c08fe4f5b7a5b166c741e4764d5e0fca1cdbf5c9
Timm <kaspth@gmail.com>
2013-11-14 12:56:03 +0100

    Added crop div to partial. Final styles for index page.

--- includes/_category.php
+++ includes/_category.php
@@ -1,7 +1,9 @@
 <?php $id = $category['id']; ?>
 <a class="category-link" href="products.php?category_id=<?php echo $id; ?>">
   <article class="category" id="<?php echo $id; ?>">
-    <img src="assets/category_images/<?php echo $id; ?>.jpg">
+    <div class="crop">
+      <img src="assets/category_images/<?php echo $id; ?>.jpg">
+    </div>
     <span class="background"><h3 class="category-title"><?php echo $category['title'] ?></h3></span>
   </article>
 </a>
