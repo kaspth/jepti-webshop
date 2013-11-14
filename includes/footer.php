1bd4535ef4b430b0b71967919ef2f832f2fce863
Timm <kaspth@gmail.com>
2013-11-14 11:16:45 +0100

    Small fixer uppery.

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,18 @@
 <footer>
-	<section class="footer_links">
-	    <ul>
-          <li><a href="om_os.php">Om os</a></li>
-          <li><a href="presse.php">Presse</a></li>
-          <li><a href="brugervilkår.php">Brugervilkår for sitet</a></li>
-          <li><a href="cookie.php">Cookie og privatlivspolitik</a></li>
-        </ul>
+	<section class="footer-links">
+	  <ul>
+      <li><a href="about_us.php">Om os</a></li>
+      <li><a href="press.php">Presse</a></li>
+      <li><a href="terms.php">Brugervilkår for sitet</a></li>
+      <li><a href="cookie_policy.php">Cookie og privatlivspolitik</a></li>
+    </ul>
 	</section>
 
-    
-    <section id="footer_adresse">Usserød Kongevej 80<br>
-      2970 Hørsholm<br>
-      Telefon: 71990905
-    </section>
+  <address class="footer">
+    Usserød Kongevej 80<br>
+    2970 Hørsholm<br>
+    Telefon: 71990905
+  </address>
 
-    <div class="copyright">
-      &copy; Jepti 2012
-    </div>
-
-
+  <div class="copyright">&copy; Jepti 2012</div>
 </footer>
