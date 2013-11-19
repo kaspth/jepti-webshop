75d510215e3d5d7b55d05a3765dffb11a4f8468a
Timm <kaspth@gmail.com>
2013-11-19 14:00:24 +0100

    Fix undefined index errors on rental page. Created message if users try to access page without being logged in.

--- rental.php
+++ rental.php
@@ -2,11 +2,13 @@
   include_once 'helpers/rentals_helper.php';
   include_once 'helpers/users_helper.php';
 
-  $renter = current_user();
-  $user_rented_from = fetch_user_by_id($_POST["user_id"]);
+  if (current_user_exists()) {
+    $renter = current_user();
+    $user_rented_from = fetch_user_by_id($_POST["user_id"]);
 
-  $rental = build_rental((int)$_POST["product_id"], $renter["id"]);
-  add_rental_to_user($user_rented_from, $rental);
+    $rental = build_rental((int)$_POST["product_id"], $renter["id"]);
+    add_rental_to_user($user_rented_from, $rental);
+  }
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,8 +21,14 @@
 
     <section class="center-section">
       <header>
-        <h1 class="title">Tak skal du have, <?php echo $renter["first_name"]; ?>.</h1>
-        <p class="subtitle">Vi har sendt en mail til <?php echo $user_rented_from["first_name"]; ?> med din forespørgsel.</p>
+        <?php if (current_user_exists()) { ?>
+          <h1 class="title">Tak skal du have, <?php echo $renter["first_name"]; ?>.</h1>
+          <p class="subtitle">Vi har sendt en mail til <?php echo $user_rented_from["first_name"]; ?> med din forespørgsel.</p>
+        <?php } else { ?>
+          <h1 class="title">Hej person vi ikke kan huske.</h1>
+          <p class="subtitle">Du er nødt til at være logget ind for at leje produkter.</p>
+          <a href="login.php">Log ind her</a>
+        <?php } ?>
       </header>
     </section>
 
