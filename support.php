16d7b444300b2255dcf623df14073ea7ced5b9bd
Timm <kaspth@gmail.com>
2013-11-19 19:28:25 +0100

    Fixed link to contact page from suppoert page.

--- support.php
+++ support.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php include 'includes/head.php'; ?>
-    <title>Support</title>
+  <?php include 'includes/head.php'; ?>
+  <title>Support</title>
 </head>
 <body>
-    <?php include 'includes/header.php'; ?>
+  <?php include 'includes/header.php'; ?>
 
-    <header class="section-header">
-        <h1 class="title">Support</h1>
-    </header>
-    <section class="support">
-        <p>
-            Vi er altid inden for rækkevidde, hvis du har spørgsmål.<br>
-            Skriv til os på <a href="">mail</a><br>
-            Ring til os på <a href="tel:71990905">71990905</a><br>
-            Hverdage mellem kl. <time>09:00</time>-<time>16:00</time>
-        </p>
+  <header class="section-header">
+    <h1 class="title">Support</h1>
+  </header>
+  <section class="support">
+    <p>
+      Vi er altid inden for rækkevidde, hvis du har spørgsmål.<br>
+      Kontakt os <a href="contact.php">her</a><br>
+      Ring til os på <a href="tel:71990905">71990905</a><br>
+      Hverdage mellem kl. <time>09:00</time>-<time>16:00</time>
+    </p>
 
-        <address>
-            JEPTI ApS<br>
-            Usserød Kongevej 80<br>
-            2979 Hørsholm
-        </address>
-    </section>
+    <address>
+        JEPTI ApS<br>
+        Usserød Kongevej 80<br>
+        2979 Hørsholm
+    </address>
+  </section>
 
-    <?php include 'includes/footer.php'; ?>
-    <?php include 'includes/scripts.php'; ?>
+  <?php include 'includes/footer.php'; ?>
+  <?php include 'includes/scripts.php'; ?>
 </body>
 </html>
