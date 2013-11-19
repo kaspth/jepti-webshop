21145ade3cbca3b2b0c9ddbb61ad78f2383e13db
Timm <kaspth@gmail.com>
2013-11-19 13:40:18 +0100

    Make index.php pass validation. Add alt attribute to img generated by category partial.

--- includes/_category.php
+++ includes/_category.php
@@ -2,7 +2,7 @@
 <a class="category-link" href="products.php?category_id=<?php echo $id; ?>">
   <article class="category" id="<?php echo $id; ?>">
     <div class="crop">
-      <img src="assets/category_images/<?php echo $id; ?>.jpg">
+      <img src="assets/category_images/<?php echo $id; ?>.jpg" alt="<?php echo $category['title']; ?>">
     </div>
     <span class="background"><h3 class="category-title"><?php echo $category['title'] ?></h3></span>
   </article>
