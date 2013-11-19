accfedd450689134a805df9e2c06f38dfd581783
Timm <kaspth@gmail.com>
2013-11-19 20:35:10 +0100

    Styled faq page.
    Conflicts:
    	assets/style.css
    	assets/style_faq.css
    	faq.php

--- faq.php
+++ faq.php
@@ -1,185 +1,191 @@
  <!DOCTYPE html>
 <html>
 <head>
-    <?php include 'includes/head.php'; ?>
-    <title>Ofte Stillede Spørgsmål</title>
-    <link href="assets/style_faq.css" rel="stylesheet" type="text/css">
+  <?php include 'includes/head.php'; ?>
+  <title>Ofte Stillede Spørgsmål</title>
 </head>
 <body>
   <?php include 'includes/header.php'; ?>
 
-  <section class="questions">
+  <section class="questions group">
     <h1 class="title">Ofte stillede spørgsmål</h1>
-    <div class="users_asks">
+
+    <section class="questions-general">
       <h3 class="subtitle">Generelle spørgsmål</h3>
-        <ul class="questions-list">
-          <li><a href="#1">Hvilke betalingsmuligheder er der?</a></li>
-          <li><a href="#2">Hvad koster det at bruge Jepti?</a></li>
-          <li><a href="#3">Er det nødvendigt med kontrakt?</a></li>
-          <li><a href="#4">Hvorfor er ikke så mange produkter på Jepti endnu?</a></li>
-        </ul>
-
-      <h3 class="subtitle">Lejer</h3>
-        <ul class="questions-list">
-          <li><a href="#5">Hvordan betaler jeg ejer?</a></li>
-          <li><a href="#6">Hvordan kan jeg se ejers kontaktinformation?</a></li>
-          <li><a href="#7">Hvorfor skal jeg registere mig?</a></li>
-          <li><a href="#8">Jeg kan ikke finde det produkt jeg skal bruge?</a></li>
-          <li><a href="#9">Hvordan udregnes prisen på leveringsservice?</a></li>
-        </ul>
-
-      <h3 class="subtitle">Ejer</h3>
-        <ul class="questions-list">
-          <li><a href="#10">Hvem kan udleje på Jepti?</a></li>
-          <li><a href="#11">Hvordan opretter jeg en annonce på sitet?</a></li>
-          <li><a href="#12">Hvad hvis mine ting går i stykker?</a></li>
-          <li><a href="#13">Kan jeg komme på sitet som professionel udlejer?</a></li>
-        </ul>
-    </div>
-
-    <section class="answers">
-
-      <article id="1" class="answer">
-        <h3>Hvilke betalingsmuligheder er der?</h3>
-        <p class="jeptis_answer">
-          Vi modtager dankort og visa/dankort på Jepti.
-        </p>
-      </article>
-
-      <article id="2" class="answer">
-        <h3>Hvad koster det at bruge Jepti?</h3>
-        <p class="jeptis_answer">
-          Det er gratis at oprette en profil og at lægge annoncer op.
-          Jepti arbejder ud fra filosofien ”No cure – no pay”,
-          hvilket betyder at du kun betaler når der foregår en reel handel
-          hvor udlejer tjener penge på handlen. Her betaler udlejer 10 % i fee til Jepti.
-          Inkluderet i det fee er en forsikring der dækker hver udlejning af et produkt med op til 100.000 DKK.
-          Det er gratis for lejer at leje igennem systemet.
-        </p>
-      </article>
-
-      <article id="3" class="answer">
-        <h3>Er det nødvendigt med kontrakt?</h3>
-        <p class="jeptis_answer">
-          Jepti tilbyder som standard en kontrakt som vi har fået udarbejdet af et af Danmarks bedste advokat huse.
-          Når du bruger standard kontrakterne udfyldes de automatisk med de rigtige informationer
-          og det er ofte meget lidt der skal tilføjes i hånden – det fremgår tydeligt af kontrakterne.
-          Kontrakterne er lavet så de beskytter både lejer og udlejer bedste muligt.
-        </p>
-      </article>
-
-      <article id="4" class="answer">
-        <h3>Hvorfor er ikke så mange produkter på Jepti endnu?</h3>
-        <p class="jeptis_answer">
-          Jepti er en ny tjeneste og vi er netop i gang med at bygge vores katalog af udlejningsprodukter op.
-          Så sidder du her og læser dette svar, så hjælp os med at komme i gang – det gør du ved at tilbyde dine ting til udlejning på sitet.
-          Det kan være alt fra højtryksrenseren du kun bruger en gang om året og traileren der står stille det meste af ugen
-          eller måske er du bare træt af at give så meget væk af din leje indtægt på dit sommerhus?
-        </p>
-      </article>
-
-      <article id="5" class="answer">
-        <h3>Hvordan betaler jeg ejer?</h3>
-        <p class="jeptis_answer">
-          På hver enkelt produkt annonce kan du se hvilke betalingsmuligheder som ejeren tilbyder.
-
-          Direkte betaling hvor du afregner med udlejer efter aftale. Typisk ved afhentning.
-          Ejeren har så listet de betalingsmuligheder han kan tilbyde.
-          Vi arbejder på livet løs på at kunne tilbyde dig Online betaling, hvor du betaler med dit kreditkort med det samme.
-        </p>
-      </article>
-
-      <article id="6" class="answer">
-        <h3>Hvordan kan jeg se ejers kontaktinformation?</h3>
-        <p class="jeptis_answer">
-          Når du søger produkter kan du se byen og en ca lokation +/-500 meter på en udlejer.
-          Du kan altid kontakte udlejer igennem vores besked system, men det er først når udlejer har godkendt din booking
-          og du selv har bekræftet den, at du får den endelige adresse sammen med lejekontrakten.
-        </p>
-      </article>
-
-      <article id="7" class="answer">
-        <h3>Hvorfor skal jeg registere mig?</h3>
-        <p class="jeptis_answer">
-          For at vi kan give den bedste og mest sikre oplevelse for både lejere og udlejere er det vigtigt
-          at vi får kontaktinformationer på de enkelte parter. Det giver til gengæld dig en række fordele:
-        </p>
-        <ul class="answers_list">
-          <li>Det sparer tid ved brug af standard kontrakter så informationerne udfyldes automatisk</li>
-          <li>Den løbende kommunikation med en potentiel udlejer er automatiseret og alle svar kommer direkte i din indbakke</li>
-          <li>Det er med til at bidrage til sikkerheden i vores system mellem lejer og udlejer</li>
-          <li>Du kan selv være udlejer. Det er blot at kigge i din hustand og se hvad du har, som du gerne vil beholde,
-              men ikke bruger så ofte. Så har du muligheden for at tjene lidt penge på dit ”lager” ved at leje det ud</li>
-        </ul>
-      </article>
-
-      <article id="8" class="answer">
-        <h3>Jeg kan ikke finde det produkt jeg skal bruge?</h3>
-        <p class="jeptis_answer">
-          Jepti er en ny tjeneste og vi er netop i gang med at bygge vores katalog af udlejningsprodukter op.
-          Så vi beklager meget hvis du ikke har fundet det du søgte.
-        </p>
-      </article>
-
-      <article id="9" class="answer">
-        <h3>Hvordan udregnes prisen på leveringsservice?</h3>
-        <p class="jeptis_answer">
-          Nogle ejere tilbyder leveringsservice hvor produktet eller servicen leveres på din adresse:
-          Det er udlejer der selv sætter prisen for leveringsservice.
-
-          Prisen er inddelt i to niveauer. Først fastsætter man en zone, som man bringer ud til for en fast pris.
-          Derefter sætter ejer den faste pris for denne startzone. Efterfølgende oplyser ejeren,
-          hvad prisen vil være for hver kilometer ud over denne zone.
-
-          Eksempel: hvis du skal betale 100 DKK for levering indenfor en radius af 5 kilometer
-          og 10 DKK for hver ekstra kilometer ud over det, så bliver prisen for en kunde, der bor 10 kilometer væk:
-          første 5 kilometer af 100 DKK + 5 (kilometer) x 10 DKK = 150 DKK.
-        </p>
-      </article>
-
-      <article id="10" class="answer">
-        <h3>Hvem kan udleje på Jepti?</h3>
-        <p class="jeptis_answer">
-          Alle kan være lejere eller udlejer på Jepti.
-          Er du professionel udlejer anbefaler vi, at du eventuelt kontakter os for en dialog om dine muligheder.
-        </p>
-      </article>
-
-      <article id="11" class="answer">
-        <h3>Hvordan opretter jeg en annonce på sitet?</h3>
-        <p class="jeptis_answer">
-          Det er gratis at oprette en profil og annoncer på sitet.
-
-          Du opretter en annonce  på følgende måde:
-        </p>
-        <ul class="answers_list">
-          <li>Du opretter en profil på Jepti</li>
-          <li>Du modtager en aktiverings email</li>
-          <li>Du får adgang til at logge ind til din personlige side.</li>
-        </ul>
-        <p class="jeptis_answer">
-          Her klikker du på "tilføj produkt" og så er du i gang.
-        </p>
-      </article>
-
-      <article id="12" class="answer">
-        <h3>Hvad hvis mine ting går i stykker?</h3>
-        <p class="jeptis_answer">
-          Heldigvis er det sjældent det sker – skulle uheldet være ude så tilbyder vores service,
-          at du kan bruge de standard kontrakter vi stiller til rådighed netop for at gøre det klart
-          hvordan en sådan situation tackles.
-
-          Det er naturligvis også muligt at bede om et depositum i forbindelse med en handel.
-        </p>
-      </article>
-
-      <article id="13" class="answer">
-        <h3>Kan jeg komme på sitet som professionel udlejer?</h3>
-        <p class="jeptis_answer">
-          Ja det er helt gratis at oprette en profil og annoncer.
-        </p>
-      </article>
+      <ul class="questions-list">
+        <li><a href="#a1">Hvilke betalingsmuligheder er der?</a></li>
+        <li><a href="#a2">Hvad koster det at bruge Jepti?</a></li>
+        <li><a href="#a3">Er det nødvendigt med kontrakt?</a></li>
+        <li><a href="#a4">Hvorfor er ikke så mange produkter på Jepti endnu?</a></li>
+      </ul>
     </section>
+
+    <section class="questions-renter">
+      <h3 class="subtitle">Lejer</h3>
+      <ul class="questions-list">
+        <li><a href="#a5">Hvordan betaler jeg ejer?</a></li>
+        <li><a href="#a6">Hvordan kan jeg se ejers kontaktinformation?</a></li>
+        <li><a href="#a7">Hvorfor skal jeg registere mig?</a></li>
+        <li><a href="#a8">Jeg kan ikke finde det produkt jeg skal bruge?</a></li>
+        <li><a href="#a9">Hvordan udregnes prisen på leveringsservice?</a></li>
+      </ul>
+    </section>
+
+    <section class="questions-owner">
+      <h3 class="subtitle">Ejer</h3>
+      <ul class="questions-list">
+        <li><a href="#a10">Hvem kan udleje på Jepti?</a></li>
+        <li><a href="#a11">Hvordan opretter jeg en annonce på sitet?</a></li>
+        <li><a href="#a12">Hvad hvis mine ting går i stykker?</a></li>
+        <li><a href="#a13">Kan jeg komme på sitet som professionel udlejer?</a></li>
+      </ul>
+    </section>
+  </section>
+
+  <section class="answers">
+    <article id="a1" class="answer">
+      <h3>Hvilke betalingsmuligheder er der?</h3>
+      <p>
+        Vi modtager dankort og visa/dankort på Jepti.
+      </p>
+    </article>
+
+    <article id="a2" class="answer">
+      <h3>Hvad koster det at bruge Jepti?</h3>
+      <p>
+        Det er gratis at oprette en profil og at lægge annoncer op.
+        Jepti arbejder ud fra filosofien ”No cure – no pay”,
+        hvilket betyder at du kun betaler når der foregår en reel handel
+        hvor udlejer tjener penge på handlen. Her betaler udlejer 10 % i fee til Jepti.
+        Inkluderet i det fee er en forsikring der dækker hver udlejning af et produkt med op til 100.000 DKK.
+        Det er gratis for lejer at leje igennem systemet.
+      </p>
+    </article>
+
+    <article id="a3" class="answer">
+      <h3>Er det nødvendigt med kontrakt?</h3>
+      <p>
+        Jepti tilbyder som standard en kontrakt som vi har fået udarbejdet af et af Danmarks bedste advokat huse.
+        Når du bruger standard kontrakterne udfyldes de automatisk med de rigtige informationer
+        og det er ofte meget lidt der skal tilføjes i hånden – det fremgår tydeligt af kontrakterne.
+        Kontrakterne er lavet så de beskytter både lejer og udlejer bedste muligt.
+      </p>
+    </article>
+
+    <article id="a4" class="answer">
+      <h3>Hvorfor er ikke så mange produkter på Jepti endnu?</h3>
+      <p>
+        Jepti er en ny tjeneste og vi er netop i gang med at bygge vores katalog af udlejningsprodukter op.
+        Så sidder du her og læser dette svar, så hjælp os med at komme i gang – det gør du ved at tilbyde dine ting til udlejning på sitet.
+        Det kan være alt fra højtryksrenseren du kun bruger en gang om året og traileren der står stille det meste af ugen
+        eller måske er du bare træt af at give så meget væk af din leje indtægt på dit sommerhus?
+      </p>
+    </article>
+
+    <article id="a5" class="answer">
+      <h3>Hvordan betaler jeg ejer?</h3>
+      <p>
+        På hver enkelt produkt annonce kan du se hvilke betalingsmuligheder som ejeren tilbyder.
+
+        Direkte betaling hvor du afregner med udlejer efter aftale. Typisk ved afhentning.
+        Ejeren har så listet de betalingsmuligheder han kan tilbyde.
+        Vi arbejder på livet løs på at kunne tilbyde dig Online betaling, hvor du betaler med dit kreditkort med det samme.
+      </p>
+    </article>
+
+    <article id="a6" class="answer">
+      <h3>Hvordan kan jeg se ejers kontaktinformation?</h3>
+      <p>
+        Når du søger produkter kan du se byen og en ca lokation +/-500 meter på en udlejer.
+        Du kan altid kontakte udlejer igennem vores besked system, men det er først når udlejer har godkendt din booking
+        og du selv har bekræftet den, at du får den endelige adresse sammen med lejekontrakten.
+      </p>
+    </article>
+
+    <article id="a7" class="answer">
+      <h3>Hvorfor skal jeg registere mig?</h3>
+      <p>
+        For at vi kan give den bedste og mest sikre oplevelse for både lejere og udlejere er det vigtigt
+        at vi får kontaktinformationer på de enkelte parter. Det giver til gengæld dig en række fordele:
+      </p>
+
+      <ul>
+        <li>Det sparer tid ved brug af standard kontrakter så informationerne udfyldes automatisk</li>
+        <li>Den løbende kommunikation med en potentiel udlejer er automatiseret og alle svar kommer direkte i din indbakke</li>
+        <li>Det er med til at bidrage til sikkerheden i vores system mellem lejer og udlejer</li>
+        <li>Du kan selv være udlejer. Det er blot at kigge i din hustand og se hvad du har, som du gerne vil beholde,
+            men ikke bruger så ofte. Så har du muligheden for at tjene lidt penge på dit ”lager” ved at leje det ud</li>
+      </ul>
+    </article>
+
+    <article id="a8" class="answer">
+      <h3>Jeg kan ikke finde det produkt jeg skal bruge?</h3>
+      <p>
+        Jepti er en ny tjeneste og vi er netop i gang med at bygge vores katalog af udlejningsprodukter op.
+        Så vi beklager meget hvis du ikke har fundet det du søgte.
+      </p>
+    </article>
+
+    <article id="a9" class="answer">
+      <h3>Hvordan udregnes prisen på leveringsservice?</h3>
+      <p>
+        Nogle ejere tilbyder leveringsservice hvor produktet eller servicen leveres på din adresse:
+        Det er udlejer der selv sætter prisen for leveringsservice.
+
+        Prisen er inddelt i to niveauer. Først fastsætter man en zone, som man bringer ud til for en fast pris.
+        Derefter sætter ejer den faste pris for denne startzone. Efterfølgende oplyser ejeren,
+        hvad prisen vil være for hver kilometer ud over denne zone.
+
+        Eksempel: hvis du skal betale 100 DKK for levering indenfor en radius af 5 kilometer
+        og 10 DKK for hver ekstra kilometer ud over det, så bliver prisen for en kunde, der bor 10 kilometer væk:
+        første 5 kilometer af 100 DKK + 5 (kilometer) x 10 DKK = 150 DKK.
+      </p>
+    </article>
+
+    <article id="a10" class="answer">
+      <h3>Hvem kan udleje på Jepti?</h3>
+      <p>
+        Alle kan være lejere eller udlejer på Jepti.
+        Er du professionel udlejer anbefaler vi, at du eventuelt kontakter os for en dialog om dine muligheder.
+      </p>
+    </article>
+
+    <article id="a11" class="answer">
+      <h3>Hvordan opretter jeg en annonce på sitet?</h3>
+      <p>
+        Det er gratis at oprette en profil og annoncer på sitet.
+
+        Du opretter en annonce  på følgende måde:
+      </p>
+
+      <ul>
+        <li>Du opretter en profil på Jepti</li>
+        <li>Du modtager en aktiverings email</li>
+        <li>Du får adgang til at logge ind til din personlige side.</li>
+      </ul>
+
+      <p>
+        Her klikker du på "tilføj produkt" og så er du i gang.
+      </p>
+    </article>
+
+    <article id="a12" class="answer">
+      <h3>Hvad hvis mine ting går i stykker?</h3>
+      <p>
+        Heldigvis er det sjældent det sker – skulle uheldet være ude så tilbyder vores service,
+        at du kan bruge de standard kontrakter vi stiller til rådighed netop for at gøre det klart
+        hvordan en sådan situation tackles.
+
+        Det er naturligvis også muligt at bede om et depositum i forbindelse med en handel.
+      </p>
+    </article>
+
+    <article id="a13" class="answer">
+      <h3>Kan jeg komme på sitet som professionel udlejer?</h3>
+      <p>
+        Ja det er helt gratis at oprette en profil og annoncer.
+      </p>
+    </article>
   </section>
 
   <?php include 'includes/footer.php'; ?>
