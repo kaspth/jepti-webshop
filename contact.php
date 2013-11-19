935112bc462470b800494cbf61ada01c0bb5a986
Timm <kaspth@gmail.com>
2013-11-19 19:45:43 +0100

    Injected styles from styles_contact in style.css.

--- contact.php
+++ contact.php
@@ -3,6 +3,7 @@
 <head>
   <?php include 'includes/head.php'; ?>
   <title>Kontakt Jepti</title>
+  <link href="assets/style_contact.css" rel="stylesheet" type="text/css">
 </head>
 <body>
   <?php include 'includes/header.php'; ?>
@@ -11,19 +12,13 @@
     <h1 class="title">Kontakt os</h1>
       <form class="contact" method="post" action="contact.php">
 
-        <label for="email">Email:</label>
-        <div class="input_field">
-          <input type="text" id="email" name="email" /><br>
-        </div>
+        <label for="email">Email:</label><br>
+        <input class="contact-field" type="text" id="email" name="email" /><br>
 
-        <label for="message">Besked:</label>
-        <div class="input_field">
-          <textarea id="message" name="message"></textarea>
-        </div>
+        <label for="message">Besked:</label><br>
+        <textarea class="contact-field" id="message" name="message" rows="5"></textarea><br>
 
-        <div class="submit_field">
-          <input type="submit" name="submit" value="Send besked"/>
-        </div>
+        <input type="submit" name="submit" value="Send besked"/>
       </form>
   </section>
 
