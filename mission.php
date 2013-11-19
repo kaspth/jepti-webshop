9b70994dd4d8287622d9efc8ffb7df79a608c558
Timm <kaspth@gmail.com>
2013-11-19 14:22:50 +0100

    Escape ampersands in SKAT link.

--- mission.php
+++ mission.php
@@ -29,7 +29,7 @@
 
     	<section class="statement">
       <p class="clause">Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-      <a class="forward-redirect" href="http://www.skat.dk/SKAT.aspx?oId=5811&vId=0&lang=DA">Læs mere om dine skattepligtige forhold på Jepti</a>
+      <a class="forward-redirect" href="http://www.skat.dk/SKAT.aspx?oId=5811&amp;vId=0&amp;lang=DA">Læs mere om dine skattepligtige forhold på Jepti</a>
     </section>
 
     <section class="center-section push-footer-down">
